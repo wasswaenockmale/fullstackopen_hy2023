c607c47a184dced792f43e1425bfac82995c0c8a
wasswaenockmale <enockmale97@gmail.com>
2023-01-29 18:07:27 +0300

    Done with course info 1.3 step3 exercise

--- part1/execpart1/src/App.js
+++ part1/execpart1/src/App.js
@@ -3,13 +3,23 @@ import Header from "./Header";
 import Total from "./Total";
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-  const exercises = [exercises1,exercises2,exercises3];
+
+  
+  const part1 = {
+    name:'Fundamentals of React',
+    exercises:10
+  };
+  const part2 = {
+    name:'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name:'State of a component',
+    exercises:14
+
+  };
+  const exercises = [part1.exercises, part2.exercises, part3.exercises];
+  
 
   return (
     <div>
