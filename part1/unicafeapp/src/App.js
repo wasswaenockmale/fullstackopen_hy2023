982af6a8fe4ab1fdd2e89b0c4424ab726604d30e
wasswaenockmale <enockmale97@gmail.com>
2023-02-01 10:50:57 +0300

    1.7 unicafe step2 exercise done

--- part1/unicafeapp/src/App.js
+++ part1/unicafeapp/src/App.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import Button from "./Button";
+import './App.css';
+
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = ()=> setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
   return (
-    <div></div>
+    <div>
+      <h1>give feedback</h1>
+      <div className="buttons">
+        <Button handleClick={handleGood} text="good"/> 
+        <Button handleClick={handleNeutral} text="neutral"/>
+        <Button handleClick={handleBad} text="bad"/>
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
   );
 }
 
---
+++ part1/unicafeapp/src/Statistics.js
@@ -0,0 +1,11 @@
+const Statistics = ([good, neutral, bad]) => {
+    return(
+        <div>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    );
+}
+
+export default Statistics;
