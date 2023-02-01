8fee75017b2e3f299df24745b0569af4968bafd7
wasswaenockmale <enockmale97@gmail.com>
2023-02-01 11:24:37 +0300

    I have done 1.8: unicafe step3 exercise

--- part1/unicafeapp/src/App.js
+++ part1/unicafeapp/src/App.js
@@ -2,6 +2,36 @@ import { useState } from "react";
 import Button from "./Button";
 import './App.css';
 
+// This is the statistics component 
+const Statistics = (props) => {
+  if(props.statistics.feedback.reduce((prev, curr) => prev + curr,0) === 0){
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>good {props.statistics.feedback[0]}</p>
+        <p>neutral {props.statistics.feedback[1]}</p>
+        <p>bad {props.statistics.feedback[2]}</p>
+        <p>all {props.statistics.feedback.reduce((prev, curr)=>prev + curr,0)}</p>
+        <p>average {0}</p>
+        <p>positive {0}%</p>
+      </div>
+    )
+  }else{
+    return(
+      <div>
+          <h1>statistics</h1>
+          <p>good {props.statistics.feedback[0]}</p>
+          <p>neutral {props.statistics.feedback[1]}</p>
+          <p>bad {props.statistics.feedback[2]}</p>
+          <p>all {props.statistics.feedback.reduce((prev, curr)=>prev + curr,0)}</p>
+          <p>average {props.statistics.average}</p>
+          <p>positive {props.statistics.percent}%</p>
+      </div>
+  );
+  }
+}
+
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -15,6 +45,12 @@ function App() {
   const average = ((good * 1) + (neutral * 0) + (bad * -1))/total;
   const percent = (good/total) * 100;
 
+  const statistics = {
+    feedback:[good, neutral, bad],
+    average: average,
+    percent: percent
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -23,13 +59,7 @@ function App() {
         <Button handleClick={handleNeutral} text="neutral"/>
         <Button handleClick={handleBad} text="bad"/>
       </div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {percent}%</p>
+      <Statistics statistics={statistics} />
     </div>
   );
 }
