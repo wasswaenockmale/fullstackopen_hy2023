41d6c3ef35074a4be0156e0177adbd09c3ebbe22
wasswaenockmale <enockmale97@gmail.com>
2023-02-23 19:43:30 +0300

    course information step 6

--- part2/execpart2/src/Course/Content.js
+++ part2/execpart2/src/Course/Content.js
@@ -2,7 +2,7 @@ import Part from "./Part";
 const Content = (props)=>{
     return(
         <>
-        {props.course.parts.map((value, index) => <Part parts = {value} key={index}/>)}
+        {props.course.parts.map((value) => <Part parts = {value} key={value.id}/>)}
         </>
     );
 }
