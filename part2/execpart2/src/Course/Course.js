629fdd6a253737af8b0f890abbf776e8c3e3a91a
wasswaenockmale <enockmale97@gmail.com>
2023-02-23 21:03:12 +0300

    I have completely seperated the module Course component from App and also inserted the rest of the module to the same component

--- part2/execpart2/src/Course/Course.js
+++ part2/execpart2/src/Course/Course.js
@@ -1,12 +1,49 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+const Part = (props)=>{
+  return(
+      <>
+          <p>{props.parts.name} {props.parts.exercises}</p>
+      </>
+  );
+}
+
+const Content = (props)=>{
+    return(
+        <>
+        {props.course.parts.map((value) => <Part parts = {value} key= {value.id}/>)}
+        </>
+    );
+}
+const Header = ({course})=>{
+  return(
+      <>
+          <h1>{course.name}</h1>
+      </>
+  );
+}
+
+const Total = ({course:{_,parts}})=>{
+  // const total = parts.reduce((prev, current)=>{
+  //     return prev + current.exercises;
+  // },0);
+  return(
+      <>
+          <h4>total of {parts.reduce((acc, current)=>{return acc + current.exercises},0)} exercises</h4>
+      </>
+  );
+}
+
 const Course = ({course})=>{
       return (
         <div>
-          <Header course = {course}/>
-          <Content course = {course}/>
-          <Total course = {course}/>
+          {course.map(c => {
+            return(
+            <div key={c.id}>
+              <Header course = {c}/>
+              <Content course = {c}/>
+              <Total course = {c}/>
+            </div>
+            );
+          })}
         </div>
       );
 }
--- part2/execpart2/src/Course/Part.js
+++
@@ -1,9 +0,0 @@
-const Part = (props)=>{
-    return(
-        <>
-            <p>{props.parts.name} {props.parts.exercises}</p>
-        </>
-    );
-}
-
-export default Part;
