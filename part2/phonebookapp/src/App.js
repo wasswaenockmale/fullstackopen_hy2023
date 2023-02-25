a82bd8237e466f07f11e39b960130d54aa8ad6be
wasswaenockmale <enockmale97@gmail.com>
2023-02-25 10:09:04 +0300

    I have completed exercise 2.8 of part 2 of fullstackopen course

--- part2/phonebookapp/src/App.js
+++ part2/phonebookapp/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const App = ()=> {
-  const [persons, setPersons] = useState([{name: "Enock Male"}]);
+  const [persons, setPersons] = useState([{name: "Enock Male", tel: "0758585390"}]);
   const [newName, setNewName] = useState('');
+  const [newTel, setNewTel] = useState('');
 
   const handleSubmit = (event)=>{
     // First prevent default
@@ -12,6 +13,7 @@ const App = ()=> {
     }else{
       const newPerson = {
         name: newName,
+        tel: newTel,
       }
       const person = [...persons].concat([newPerson]);
       setPersons(person);
@@ -19,16 +21,24 @@ const App = ()=> {
     event.preventDefault();
   }
 
-  const handlChange = (event)=>{
+  const handleNameChange = (event)=>{
     event.preventDefault();
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event)=>{
+    event.preventDefault();
+    setNewTel(event.target.value);
   }
   return (
     <div>
       <h2>PhoneBook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handlChange}/>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,7 +47,7 @@ const App = ()=> {
       <h2>Numbers</h2>
       <div>
         {
-          persons.map(person => <p key={person.name}>{person.name}</p>)
+          persons.map(person => <p key={person.name}>{person.name} {person.tel}</p>)
         }
       </div>
     </div>
