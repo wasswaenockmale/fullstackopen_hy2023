6be2e7816741893ea53ee1f66682176a12a10b0e
wasswaenockmale <enockmale97@gmail.com>
2023-02-25 09:55:02 +0300

    I have completed exercise 2.6 of part 2

--- part2/phonebookapp/src/App.js
+++ part2/phonebookapp/src/App.js
@@ -1,8 +1,39 @@
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = ()=> {
+  const [persons, setPersons] = useState([{name: "Enock Male"}]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    }
+    const person = [...persons].concat([newPerson]);
+    setPersons(person);
+  }
+
+  const handlChange = (event)=>{
+    event.preventDefault();
+    setNewName(event.target.value);
+  }
   return (
     <div>
+      <h2>PhoneBook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handlChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(person => <p key={person.name}>{person.name}</p>)
+        }
+      </div>
     </div>
   );
 }
