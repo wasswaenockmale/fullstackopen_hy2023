1a09b8d2667b3f92739ee143cb07c4ceda594b66
wasswaenockmale <enockmale97@gmail.com>
2023-03-04 02:15:03 +0300

    I have completed Data for countries step1 of part 2 final exercise

--- part2/countriesdataapp/src/App.js
+++ part2/countriesdataapp/src/App.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Notification from "./components/Nitification";
+
+
 const App = ()=> {
+  const [value, setValue] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  const handleChange = (event) =>{
+    event.preventDefault();
+    setValue(event.target.value);
+  }
+
+  useEffect(()=>{
+    // Whenever the value changes, show more countries.
+    axios.get(`https://restcountries.com/v2/all`)
+    .then(response => {
+      console.log(response.data);
+      setCountries(response.data.filter(element => element.name.includes(value)))
+    })
+    .catch(error => {
+      console.log("This is an error here",error);
+    });
+  }, [value]);
+
+  // console.log(countries)
   return (
     <div className="App">
-     
+      find countries<input onChange={handleChange}/>
+      <Notification countries={countries} />
     </div>
   );
 }
---
+++ part2/countriesdataapp/src/components/Nitification.js
@@ -0,0 +1,42 @@
+const Notification = ({countries, value}) =>{
+    if(countries.length <= 10){
+        if(countries.length === 1){
+            return (
+                <div>
+                    <h1>{countries[0].name}</h1>
+                    <div>{countries[0].capital}</div>
+                    <div>{countries[0].area}</div>
+                    <h3>Languages</h3>
+                    <ul>
+                        {countries[0].languages.map((lang,index) => 
+                            <li key={index}>{lang.name}</li>
+                        )}
+                    </ul>
+                    <img src={countries[0].flag} alt="Country flag"/>
+                </div>
+            )
+        }else if(countries.length === 0){
+            return null;
+        }else{
+            return (
+                <div>
+                    {countries.map(country => <div key={country.name}>
+                        {country.name}
+                    </div>)}
+                </div>
+            )
+        }
+    }else{
+        if(value === ''){
+            return null
+        }else{
+            return(
+                <div>
+                    To many matches, specify another filter
+                </div>
+            )
+        }
+    }
+}
+
+export default Notification;
